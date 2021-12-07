466a82eaa842ab57d049617e31721bed6ff77294
tinutmap <tinutmap@gmail.com>
2021-12-07 11:44:04 -0500

    q3
    Revisit q3 to better practice sliding windows
    Rewrite q3 test

--- q3/q3.py
+++ q3/q3.py
@@ -1,17 +1,22 @@
 def lengthOfLongestSubstring(s: str) -> int:
-    # # first solution
-    # max_length = 1
-    # for i in range(len(s)):
-    #     l = [s[i]]
-    #     for j in s[i+1::]:
-    #         if j not in l:
-    #             l.append(j)
-    #         else:
-    #             break
-    #     if len(l) > max_length:
-    #         max_length = len(l)
-    # return max_length
+    """
+    # solution 1: failed
+    max_length = 1
+    for i in range(len(s)):
+        l = [s[i]]
+        for j in s[i+1::]:
+            if j not in l:
+                l.append(j)
+            else:
+                break
+        if len(l) > max_length:
+            max_length = len(l)
+    return max_length
 
+    """
+    """
+    # solution 2: passed, not so good time
+    # https://leetcode.com/submissions/detail/597969245/
     max_len = 0
     max_sub_st = []
     for char in s:
@@ -25,20 +30,59 @@ def lengthOfLongestSubstring(s: str) -> int:
             if len(max_sub_st) > max_len:
                 max_len = len(max_sub_st)
     return max_len
+    """
 
+    """
     # most voted solution
-    # start = maxLength = 0
-    # usedChar = {}
+    start = maxLength = 0
+    usedChar = {}
 
-    # for i in range(len(s)):
-    #     if s[i] in usedChar and start <= usedChar[s[i]]:
-    #         start = usedChar[s[i]] + 1
-    #     else:
-    #         maxLength = max(maxLength, i - start + 1)
+    for i in range(len(s)):
+        if s[i] in usedChar and start <= usedChar[s[i]]:
+            start = usedChar[s[i]] + 1
+        else:
+            maxLength = max(maxLength, i - start + 1)
 
-    #     usedChar[s[i]] = i
+        usedChar[s[i]] = i
 
-    # return maxLength
+    return maxLength
+    """
+
+    """
+    # solution 3: accepted, still not so great
+    # https://leetcode.com/submissions/detail/598391331/
+    max_l = 0
+    max_st = ''
+    for f in s:
+        if f not in max_st:
+            max_st = max_st+f
+        else:
+            j = max_st.find(f)
+            max_st = max_st[j+1::]+f
+        max_l = max(max_l, len(max_st))
+    return max_l
+    """
+
+    # solution 4: using dict instead of max_st
+    # still similar performance to solution 3, not really improving
+    # https://leetcode.com/submissions/detail/598403168/
+    d = {}
+    j = 0
+    max_length = 0
+    length = 0
+    for l in s:
+        if l not in d or d[l] == 0:
+            d[l] = 1
+            length += 1
+        else:
+            while s[j] != l:
+                d[s[j]] = 0
+                j += 1
+                length -= 1
+            # d[l] = 1
+            j += 1
+        max_length = max(max_length, length)
+    return max_length
 
 
-lengthOfLongestSubstring("pwwkew")
+# lengthOfLongestSubstring("pwwkew")
--- q3/test_3.py
+++ q3/test_3.py
@@ -1,9 +1,35 @@
 from q3 import lengthOfLongestSubstring as f
 
 
-def test_f():
+def test_1():
+    assert f("tmmzuxt") == 5
+
+
+def test_2():
     assert f('abcabcbb') == 3
+
+
+def test_3():
     assert f('bbbbb') == 1
+
+
+def test_4():
     assert f('pwwkew') == 3
+
+
+def test_5():
     assert f('') == 0
+
+
+def test_6():
     assert f(' ') == 1
+
+
+"""
+"tmmzuxt"
+"abcabcbb"
+"bbbbb"
+"pwwkew"
+""
+" "
+"""
